0566e355ba4480a2061d5d5c622a17cee1ee2358
Лев Верёвкин <jdjfh8w@gmail.com>
2024-12-13 13:27:51 +0300

    изменение кода

--- scr/widget.py
+++ scr/widget.py
@@ -1,4 +1,14 @@
+from scr import get_mask_card_number, get_mask_account
+
+
 def mask_account_card(*infa):
-    print(infa)
-    pass
-mask_account_card('Visa Platinum', 7000792289606361)
+    infa = list(infa)
+    if infa[0] != 'Счёт' or infa[0] != 'Счет':
+        infa[1] = get_mask_card_number(str(infa[1]))
+        return f'{infa[0]} {infa[1]}'
+    infa[1] = get_mask_account(str(infa[1]))
+    return f'{infa[0]} {infa[1]}'
+
+
+print(mask_account_card("Visa Platinum", 7000792289606361))
+print(mask_account_card('Счет', 73654108430135874305))
