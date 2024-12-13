f5cd1daf29646ea355b81df070824ca3e2afd13d
Лев Верёвкин <jdjfh8w@gmail.com>
2024-12-13 13:57:42 +0300

    финальная версия

--- scr/widget.py
+++ scr/widget.py
@@ -1,14 +1,20 @@
 from scr import get_mask_card_number, get_mask_account
 
 
-def mask_account_card(*infa):
-    infa = list(infa)
-    if infa[0] != 'Счёт' or infa[0] != 'Счет':
-        infa[1] = get_mask_card_number(str(infa[1]))
-        return f'{infa[0]} {infa[1]}'
-    infa[1] = get_mask_account(str(infa[1]))
-    return f'{infa[0]} {infa[1]}'
+def mask_account_card(infa: str) -> str:
+    lis_ = infa.split()
+    if 'счет' in ' '.join(lis_[:-1]).lower():
+        lis_[-1] = get_mask_account(lis_[-1])
+        return ' '.join(lis_)
+    lis_[1] = get_mask_card_number(lis_[-1])
+    return ' '.join(lis_)
 
 
-print(mask_account_card("Visa Platinum", 7000792289606361))
-print(mask_account_card('Счет', 73654108430135874305))
+def get_date(infa: str) -> str:
+    data = infa[:10].split("-")
+    return f"{data[2]}-{data[1]}-{data[0]}"
+
+
+print(mask_account_card("Visa Platinum 7000792289606361"))
+print(mask_account_card("Счет 73654108430135874305"))
+print(get_date("2024-12-10T02:26:18.671407"))
